a02df81b57f174dbbddef20a69b598a148468dd4
fhemery <fhemery@hemit.fr>
2022-02-18 17:26:23 +0100

    Create a java version

---
+++ java/src/main/java/org/hemit/model/TournamentToCreate.java
@@ -0,0 +1,13 @@
+package org.hemit.model;
+
+
+public class TournamentToCreate {
+    public String name;
+
+    public TournamentToCreate() {
+    }
+
+    public TournamentToCreate(String name) {
+        this.name = name;
+    }
+}
