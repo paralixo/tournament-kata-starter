2f9ea0ab3aadcc95f87f4d2cb53c106521237225
Benoit Thrive <benoit@agencethrive.fr>
2022-03-03 17:53:36 +0100

    Refacto

--- apps/tournament/src/app/api/tournament-api.ts
+++ apps/tournament/src/app/api/tournament-api.ts
@@ -9,19 +9,15 @@ export const postTournament = (req: Request, res: Response) => {
   const tournamentToAdd: TournamentToAdd = req.body;
 
   if (!tournamentToAdd.name || tournamentToAdd.name == '') {
-    res.status(400);
-    res.send({error: 'Le champ nom est manquant ou vide.'})
+    respond(res, 400, {error: 'Le champ nom est manquant ou vide.'})
   }
   if (tournamentRepository.getTournamentByName(tournamentToAdd.name)) {
-    res.status(400);
-    res.send({error: 'Le nom est déjà pris.'})
+    respond(res, 400, {error: 'Le nom est déjà pris.'})
   }
 
   const tournament = { id: uuidv4(), name: tournamentToAdd.name, phases: [], participants: [] };
   tournamentRepository.saveTournament(tournament);
-
-  res.status(201);
-  res.send({ id: tournament.id });
+  respond(res, 201, {id: tournament.id})
 };
 
 export const getTournament = (req: Request, res: Response) => {
@@ -29,12 +25,10 @@ export const getTournament = (req: Request, res: Response) => {
 
   const tournament = tournamentRepository.getTournament(id);
   if (tournament == null) {
-    res.status(404);
-    res.send({error: "Ce tournoi n'existe pas"})
+    respond(res, 404, {error: "Ce tournoi n'existe pas"})
   }
 
-  res.status(200);
-  res.send(tournament);
+  respond(res, 200, tournament)
 };
 
 export const postParticipant = (req: Request, res: Response) => {
@@ -42,26 +36,22 @@ export const postParticipant = (req: Request, res: Response) => {
   const participant:Participant = {id: uuidv4(), ...req.body};
 
   if(!participant.name || participant.name == ''){
-    res.status(400)
-    res.send({error: "Le nom ne peut pas être vide"})
+    respond(res, 400, {error: "Le nom ne peut pas être vide"})
   }
 
   if(!participant.elo || !Number.isInteger(participant.elo)){
-   res.status(400)
-   res.send({error: "elo doit être un nombre entier"})
+    respond(res, 400, {error: "elo doit être un nombre entier"})
   }
 
   const tournament = tournamentRepository.getTournament(id);
   if (tournament == null){
-    res.status(404)
-    res.send({error: "Le tournoi n'existe pas"})
+    respond(res, 404, {error: "Le tournoi n'existe pas"})
   }
 
   const participantAlreadyExists = tournament.participants.find(p => p.name == participant.name) != undefined
 
   if(participantAlreadyExists){
-    res.status(400)
-    res.send({error: "Le participant existe déjà"})
+    respond(res, 400, {error: "Le participant existe déjà"})
   }
 
   const participants:Participant[] = tournament.participants
@@ -69,8 +59,7 @@ export const postParticipant = (req: Request, res: Response) => {
 
   tournamentRepository.saveTournament(tournament);
 
-  res.status(201);
-  res.send({ id: participant.id });
+  respond(res, 201, {id: participant.id})
 };
 
 export const getParticipant = (req: Request, res: Response) => {
@@ -78,10 +67,13 @@ export const getParticipant = (req: Request, res: Response) => {
 
   const tournament = tournamentRepository.getTournament(id);
   if (tournament == null) {
-    res.status(404)
-    res.send({error: "Le tournoi n'existe pas"})
+    respond(res, 404, {error: "Le tournoi n'existe pas"})
   }
 
-  res.status(200);
-  res.send(tournament.participants)
+  respond(res, 200, tournament.participants)
 };
+
+const respond = (res: Response, code: number, body: any) => {
+  res.status(code)
+  res.send(body)
+}
